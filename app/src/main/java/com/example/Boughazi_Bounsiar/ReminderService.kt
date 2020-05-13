5dd9ae10a6a0b71927c6254604aa11360e7bc47b
LinaBghz <gl_boughazi@esi.com>
2020-05-13 23:57:01 +0100

    Notification fixed

--- app/src/main/java/com/example/Boughazi_Bounsiar/ReminderService.kt
+++ app/src/main/java/com/example/Boughazi_Bounsiar/ReminderService.kt
@@ -1,12 +1,12 @@
 package com.example.Boughazi_Bounsiar
 
 
+
 import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
-import androidx.core.app.NotificationCompat
 import com.example.Boughazi_Bounsiar.R.*
 import java.util.*
 
@@ -14,55 +14,58 @@ class ReminderService:Service(){
 
     private var player: MediaPlayer? = null
     private val timer: Timer = Timer()
-
+    private var prayer : String = ""
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val alarmTime = intent!!.getStringExtra("Time")
-        val prayer = intent!!.getStringExtra("Prayer")
+        prayer = intent!!.getStringExtra("Prayer")
         player = MediaPlayer.create(this, raw.adhan)
         player!!.isLooping = true
 
-        val notificationIntent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0, notificationIntent, 0
-        )
-        val notif : Notification
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            val mChannel = NotificationChannel(
-                "ch006", "ch006", NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationManager.createNotificationChannel(mChannel)
-           notif = NotificationCompat.Builder(this, "ch006")
-                .setSmallIcon(drawable.ic_launcher_background)
-                .setContentTitle("Reminder for prayer")
-                .setContentText("Time to pray" + prayer)
-                .setContentIntent(pendingIntent)
-                .build()
-            notificationManager.notify(1, notif)
-        }
-        else {
-          notif = Notification.Builder(this)
-                .setContentTitle("Prayer")
-                .setContentTitle("Reminder for prayer")
-                .setContentText("Time to pray" + prayer).setSmallIcon(drawable.ic_launcher_background)
-                .setContentIntent(pendingIntent).setAutoCancel(true)
-                .setOngoing(true)
-                .build()
-            notificationManager.notify(1, notif)
-        }
-            timer.scheduleAtFixedRate(object : TimerTask() {
-                override fun run() {
-                    val currentTime = Calendar.getInstance().time.hours.toString() + ":" +  Calendar.getInstance().time.minutes.toString()
-                    if (currentTime==alarmTime){
-                        startForeground(1, notif)
-                        notificationManager.notify(1,notif)
-                        player!!.start()
-                    } else { }
-                }
-            }, 0, 2000)
+        val pIntent = PendingIntent.getService(this, System.currentTimeMillis().toInt(), intent!!, 0)
+
+        val builder = builder(notificationManager, pIntent)
+        var notif = builder!!.build()
+
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val currentTime = Calendar.getInstance().time.hours.toString() + ":" +  Calendar.getInstance().time.minutes.toString()
+                if (currentTime==alarmTime){
+                    startForeground(1, notif)
+                    player!!.start()
+                } else { }
+            }
+        }, 0, 2000)
+
 
         return START_STICKY
+    }
+
+    private fun builder(nm: NotificationManager, pi: PendingIntent): Notification.Builder? {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val mChannel = NotificationChannel(
+                "ch00", "ch00", NotificationManager.IMPORTANCE_HIGH)
+            nm.createNotificationChannel(mChannel)
+            val builder = Notification.Builder(this,"ch00")
+                .setContentTitle("Prayer Reminder")
+                .setContentText("It's time to pray "+prayer).setSmallIcon(drawable.prayer)
+                .setContentIntent(pi).setAutoCancel(true)
+                .setAutoCancel(true)
+            return builder
+
+
+        }else{
+
+            val builder = Notification.Builder(this)
+                .setContentTitle("Prayer Reminder")
+                .setContentText("Time to pray" + prayer).setSmallIcon(drawable.prayer)
+                .setContentIntent(pi).setAutoCancel(true)
+                .setOngoing(true)
+                .setAutoCancel(true)
+            return builder
+
+        }
+
     }
 
     override fun onBind(intent: Intent?): IBinder? { return null }
@@ -70,5 +73,6 @@ class ReminderService:Service(){
         super.onDestroy()
         player!!.stop()
     }
+
 
 }
